edefc3913bb2a762df8bca85726fbe8c1753de9d
pasky <792666942@qq.com>
2019-05-31 00:06:21 +0800

    2019/5/31

--- www/orm.py
+++ www/orm.py
@@ -58,3 +58,21 @@ async def execute(sql, args, autocommit=True):
                 await conn.rollback()
             raise
         return affected
+
+# orm
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ','.join(L)
+
+class Field(object):
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_tpye
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
